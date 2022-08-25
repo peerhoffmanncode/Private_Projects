129adaf73ce3a6fabfb1bafad085049833538e4b
peerhoffmanncode <peerhoffmann81@gmail.com>
2022-08-25 18:12:29 +0200

    updated tictactoe

--- TicTacToe_TestProject/KI.py
+++ TicTacToe_TestProject/KI.py
@@ -20,9 +20,9 @@ def can_make_three(position, player_symbol, gamestate):
     '''
 
     which position to check in 3 in a ROW is possible :param position:
-    which symbole to check for                        :param player_symbol:
+    which symbol to check for                         :param player_symbol:
     List to check                                     :param gamestate:
-    retrun which position to use                      :return:
+    return which position to use                      :return:
 
     check for each position if a win is possible for CPU or Opponent
 
@@ -40,9 +40,9 @@ def can_make_two(position, player_symbol, gamestate):
     '''
 
     which position to check in 3 in a ROW is possible :param position:
-    which symbole to check for                        :param player_symbol:
+    which symbol to check for                         :param player_symbol:
     List to check                                     :param gamestate:
-    retrun which position to use                      :return:
+    return which position to use                      :return:
 
     check for each position is useful to build a row if TWO
 
@@ -118,10 +118,10 @@ def can_make_two(position, player_symbol, gamestate):
 def find_best_choise(player_symbol, opponent_symbol, gamestate):
     '''
 
-    which symbole uses the player/cpu                 :param player_symbol:
-    which symbole uses the Opponent                   :param opponent_symbol::
+    which symbol uses the player/cpu                  :param player_symbol:
+    which symbol uses the Opponent                    :param opponent_symbol::
     List to check                                     :param gamestate:
-    retrun which position to use                      :return:
+    return which position to use                      :return:
 
     Main routine to decide to steps of the CPU
 
@@ -139,13 +139,13 @@ def find_best_choise(player_symbol, opponent_symbol, gamestate):
         von = 0
         bis = 9
 
-    # is somehow a cpu win possible? First interesst!
+    # is somehow a cpu win possible? First interest!
     for i in range(von, bis, random_value):
         if can_make_three(i, player_symbol, gamestate):
-            print ("cpu win with: " + str(i+1))
+            print("cpu win with: " + str(i + 1))
             return i + 1
 
-    # Somehow Opp win possible? Needed to be blocked is second interesst
+    # Somehow Opp win possible? Needed to be blocked is second interest
     for i in range(von, bis, random_value):
         if can_make_three(i, opponent_symbol, gamestate):
             print("opp win with: " + str(i + 1))
@@ -159,13 +159,13 @@ def find_best_choise(player_symbol, opponent_symbol, gamestate):
 
     # decide if CPU wants to use center cell (5)
     if gamestate[4] == " " and random_value > 0:
-        print("cpu uses cell: " + str(5))
+        print("cpu uses center cell !")
         return 5
 
     # Place at any free cell
     for i in range(von, bis, random_value):
         if gamestate[i] == " ":
-            print("cpu uses random cell: " + str(i+1))
+            print("cpu uses random cell: " + str(i + 1))
             return i + 1
 
     return -1
